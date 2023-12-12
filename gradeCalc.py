f4b68ba861975bacaf717e51b46020a80246f800
Mathias <mrseipa@gmail.com>
2023-12-12 14:17:20 +0100

    Added feature: Actual grades vs wishful grades.

--- gradeCalc.py
+++ gradeCalc.py
@@ -1,13 +1,25 @@
-grades = {
+actual_grades = {
     "Elektriske Kretser": "E",
     "Matte 1000": "C",
-    "Programmering 1": "B",
+    "Programmering 1": "A",
     "Elektronikk": "A",
     "Digitalteknikk": "D",
     "Fysikk": "E",
-    "Programmering 2": "",
+    "Programmering 2": "A",
     "Matte 2000": "B", 
     "Dynamiske Systemer": ""
+}
+
+wishful_grades = {
+    "Elektriske Kretser": "C",
+    "Matte 1000": "C",
+    "Programmering 1": "A",
+    "Elektronikk": "A",
+    "Digitalteknikk": "D",
+    "Fysikk": "D",
+    "Programmering 2": "A",
+    "Matte 2000": "B", 
+    "Dynamiske Systemer": "D"
 }
 
 gradeConversion = [
@@ -20,13 +32,13 @@ gradeConversion = [
     ]
 
 #Makes a list with numerical grades from the letter grades.
-def grade_calc():
+def grade_calc(list_of_grades):
     calculation_list = []
-    for i in grades.values():
+    for i in list_of_grades.values():
         for o in gradeConversion:
             if i.upper() in o:
                 calculation_list.append(o[1])
-    return sum(calculation_list) / len(calculation_list)
+    return round(sum(calculation_list) / len(calculation_list), 3)
 
 def num_to_letter(num_grade):
     num_grade = round(num_grade)
@@ -34,5 +46,8 @@ def num_to_letter(num_grade):
         if num_grade in i:
             return i[0]
 
-print(f"The numerical grade is: {grade_calc()}")
-print(f"This rounds to a letter grade of: {num_to_letter(grade_calc())}")
+print(f"The numerical grade is: {grade_calc(actual_grades)}")
+print(f"This rounds to a letter grade of: {num_to_letter(grade_calc(actual_grades))}")
+print()
+print(f"The wishful grade is: {grade_calc(wishful_grades)}")
+print(f"This rounds to a letter grade of: {num_to_letter(grade_calc(wishful_grades))}")
